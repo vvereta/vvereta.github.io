e0f5fc9f51a0b4f3db540eb3219e665e90911e3d
Vitalii Vereta <vitalii.vereta@volvocars.com>
2019-09-11 17:56:11 +0300

    Implement func get_duration()

--- src/dates.js
+++ src/dates.js
@@ -1,4 +1,9 @@
 function get_duration() {
-    var a = "1 yr 4 mos"
-    document.getElementById("years").innerText = a;
+    var now = new Date();
+    var start = new Date(2018, 4, 1);
+    var diff = new Date(now.getTime() - start.getTime());
+    var years = diff.getUTCFullYear() - 1970;
+    var month = diff.getUTCMonth();
+    var result = years + " yr " + month + " mos";
+    document.getElementById("years").innerText = result;
    }
