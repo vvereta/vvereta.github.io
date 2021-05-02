7dafb4cda1a434cc01ae01bc97447881ae865966
Vitalii Vereta <vitalii.vereta@volvocars.com>
2021-05-02 13:03:34 +0300

    Add new position

--- src/dates.js
+++ src/dates.js
@@ -1,6 +1,6 @@
 function get_duration() {
     var now = new Date();
-    var start = new Date(2018, 4, 1);
+    var start = new Date(2020, 12, 1);
     var diff = new Date(now.getTime() - start.getTime());
     var years = diff.getUTCFullYear() - 1970;
     var month = diff.getUTCMonth();
