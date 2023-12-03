081c3485fa12916fc7233bd0e44790f29a0cc7c6
Vitalii <guninyourassh0le@gmail.com>
2023-12-03 15:54:23 +0200

    Update dates.js

--- src/dates.js
+++ src/dates.js
@@ -1,6 +1,6 @@
-function get_duration() {
+function get_duration(y, m, d) {
     var now = new Date();
-    var start = new Date(2020, 12, 1);
+    var start = new Date(y, m, d);
     var diff = new Date(now.getTime() - start.getTime());
     var years = diff.getUTCFullYear() - 1970;
     var month = diff.getUTCMonth();
